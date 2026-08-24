f8f7d27991308303c9c92206d8a5493f5ffd5a39
Fabio Landin <flandin1990@gmail.com>
2026-08-24 10:05:57 -0300

    chore: configs inicias do projeto

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-red-800">TaskFlow</h1>
+      <Button variant="default">Olá</Button>
+    </div>
+  )
+}
+
+export default App
