1eaaa734e995f11dafb6a44220dd64eb2ca865fa
Fabio Landin <flandin1990@gmail.com>
2026-08-25 11:22:24 -0300

    feat: configurações iniciais de rotas e toast do sonner

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
+import './index.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import Login from './pages/login.tsx'
 import App from './App.tsx'
-import './index.css'
+import { Toaster } from 'sonner'
+import Projetos from './pages/projetos.tsx'
+import MinhasTarefas from './pages/minhas-tarefas.tsx'
+import Labels from './pages/labels.tsx'
+import Usuarios from './pages/usuarios.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+    <Routes>
+      <Route>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/minhas-tarefas" element={<MinhasTarefas />} />
+        <Route path="/projetos" element={<Projetos />} />
+        <Route path="/labels" element={<Labels />} />
+        <Route path="/usuarios" element={<Usuarios />} />
+      </Route>
+    </Routes>
+    </BrowserRouter>
+    <Toaster />
+  </StrictMode>
 )
