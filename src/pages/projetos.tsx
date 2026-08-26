91d7eee6c6185ccfc638ddb85b9bbcab9fdfdd3f
Fabio Landin <flandin1990@gmail.com>
2026-08-26 14:37:15 -0300

    feat: criando pagina de projetos

--- src/pages/projetos.tsx
+++ src/pages/projetos.tsx
@@ -1,8 +1,25 @@
+import DialogNewProjeto from "@/components/DialogNewProjeto";
+import Layout from "@/components/Layout";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+
 const Projetos = () => {
+
+    //States para o dialog
+    const [open, setOpen] = useState(false);
+
     return (
-        <div>
-            <h1>Pagina de Projetos</h1>
-        </div>
+        <Layout>
+            <div className="flex flex-row">
+            <h1 className="text-2xl font-bold text-white">Projetos</h1>
+            <Button variant="destructive" className="ml-auto" onClick={() => setOpen(true)}>
+                Novo Projeto
+            </Button>
+            </div>
+
+
+            <DialogNewProjeto open={open} onOpenChange={setOpen} />
+        </Layout>
     );
 };
 
