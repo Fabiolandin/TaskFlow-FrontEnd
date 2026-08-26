4cd7273c67bc365291307553199adad3881f168f
Fabio Landin <flandin1990@gmail.com>
2026-08-26 14:36:43 -0300

    feat: criando layout básico para o sistema

---
+++ src/components/Layout.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from "react";
+import Sidebar from "@/components/Sidebar";
+
+interface LayoutProps {
+    children: ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps) {
+    return (
+        <div className="flex h-screen overflow-hidden bg-[#0B0C0E]">
+            <Sidebar />
+            <main className="flex-1 overflow-y-auto p-6">
+                {children}
+            </main>
+        </div>
+    );
+}
