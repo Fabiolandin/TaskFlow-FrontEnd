bdd6fb3fa6c49929d69fdbdc57fdc9d4b4b21bf2
Fabio Landin <flandin1990@gmail.com>
2026-08-31 22:05:07 -0300

    feat: adicionando layout em todas as pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
+import Layout from "./components/Layout"
 import { Button } from "./components/ui/button"
 
 function App() {
 
 
   return (
-    <div>
+    <Layout>
       <h1 className="text-3xl font-bold text-red-800">TaskFlow</h1>
       <Button variant="default">Olá</Button>
-    </div>
+    </Layout>
   )
 }
 
--- src/pages/minhas-tarefas.tsx
+++ src/pages/minhas-tarefas.tsx
@@ -1,8 +1,10 @@
+import Layout from "@/components/Layout";
+
 const MinhasTarefas = () => {
     return (
-        <div>
+        <Layout>
             <h1>Pagina de MinhasTarefas</h1>
-        </div>
+        </Layout>
     );
 };
 
