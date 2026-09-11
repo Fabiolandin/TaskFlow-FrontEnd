421783b3f41bb745d66eced1ee5bee54d1bcf3be
Fabiolandin <flandin1990@gmail.com>
2026-09-11 14:24:58 -0300

    finalizando módulo de usuarios

--- src/pages/usuarios.tsx
+++ src/pages/usuarios.tsx
@@ -3,25 +3,80 @@ import Layout from "@/components/Layout";
 import { Button } from "@/components/ui/button";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { Pencil, XIcon } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { toast } from "sonner";
-import UsuariosMock, { type UsuarioMock } from "@/usuarios";
 import DialogUsuario from "@/components/DialogNewUsuario";
+import { createUsuario, deleteUsuario, fetchUsuarios, updateUsuario } from "@/fetchs/fetchUsuarios";
+import DialogDetailsUsuario from "@/components/DialogDetailsUsuario";
+
+export interface Usuario {
+    id: number;
+    nome: string;
+    email: string;
+}
 
 const Usuarios = () => {
 
+
     //state para abrir dialog de criação
     const [open, setOpen] = useState(false)
+    const [openDetails, setOpenDetails] = useState(false)
 
-   const [usuarios, setUsuarios] = useState<UsuarioMock[]>(UsuariosMock)
+    //State para receber usuários
+    const [usuarios, setUsuarios] = useState<Usuario[]>([])
+    const [usuarioSelecionado, setUsuarioSelecionado] = useState<Usuario | null>(null)
 
-    const deletUsuario = async (id) => {
+    const getDados = async () => {
         try {
+            const resposta = await fetchUsuarios()
+            setUsuarios(resposta)
+        } catch (error) {
+            toast.error("Erro ao buscar usuários")
+        }
+    }
+
+    const criarUsuario = async (nome: string, email: string, senha: string) => {
+        try {
+            await createUsuario(nome, email, senha)
+            toast.success("Usuário criado com sucesso")
+            await getDados() // Atualiza a lista de usuários após a criação
+        } catch (error) {
+            toast.error("Erro ao criar usuário")
+            throw error
+        }
+    }
+
+    const editarUsuario = async (id: number, nome: string, email: string, senha?: string) => {
+        try{
+            await updateUsuario(id, nome, email, senha)
+            toast.success("Usuário atualizado com sucesso")
+            await getDados() // Atualiza a lista de usuários após a atualização
+        } catch(error){
+            toast.error("Erro ao atualizar usuário")
+            throw error
+        }
+    }
+
+    const deletUsuario = async (id: number) => {
+        try {
+            await deleteUsuario(id)
             toast.success("Usuário deletado com sucesso")
+            await getDados() // Atualiza a lista de usuários após a deleção
         } catch (error) {
             toast.error("Usuário não foi deletado")
         }
     }
+
+    //funcao para abrir dialog e mandar usuario selecionado
+    const handleDialogOpen = (usuario: Usuario) => {
+        setUsuarioSelecionado(usuario);
+        setOpenDetails(true);
+    }
+
+    useEffect(() => {
+        getDados()
+    }, [])
+
     return (
         <Layout>
             <div className="flex flex-row">
@@ -48,8 +103,8 @@ const Usuarios = () => {
                                 <TableCell className="font-medium text-white">{usuario.nome}</TableCell>
                                 <TableCell className="font-mono text-zinc-400">{usuario.email}</TableCell>
                                 <TableCell className="text-right">
-                                    <Button variant="ghost" size="icon-sm" className="text-zinc-400 hover:text-white">
-                                        <Pencil className="size-4" />
+                                    <Button variant="ghost" size="icon-sm" className="text-zinc-400 hover:text-white" onClick={() => handleDialogOpen(usuario)}>
+                                        <Pencil className="size-4"/>
                                     </Button>
 
                                     <ConfirmDeleteDialog
@@ -73,7 +128,8 @@ const Usuarios = () => {
 
 
 
-            <DialogUsuario open={open} onOpenChange={setOpen} />
+            <DialogUsuario open={open} onOpenChange={setOpen} onCreateUsuario={criarUsuario} />
+            <DialogDetailsUsuario open={openDetails} onOpenChange={setOpenDetails} usuarioSelecionado={usuarioSelecionado} onEditUsuario={editarUsuario} />
         </Layout>
     );
 };
