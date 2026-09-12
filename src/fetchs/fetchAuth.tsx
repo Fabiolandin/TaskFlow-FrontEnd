e60e0f3a31d2270ac388b79885d3d403d4e0afb2
Fabiolandin <flandin1990@gmail.com>
2026-09-12 13:06:59 -0300

    feat: adicionando tipagem para o fetch de autenticação

--- src/fetchs/fetchAuth.tsx
+++ src/fetchs/fetchAuth.tsx
@@ -1,7 +1,8 @@
 import { apiClient } from "@/api/apiClient";
+import type { LoginResponse } from "@/pages/login";
 
 export const login = async (email: string, senha: string) => {
-    return apiClient(`/auth/login`, {
+    return apiClient<LoginResponse>(`/auth/login`, {
         method: "POST",
         body: JSON.stringify({ email, senha }),
     });
