fdcd6d9c60be851bbe00d34cca85cca01eaa6427
Fabiolandin <flandin1990@gmail.com>
2026-09-12 13:45:00 -0300

    feat: criando fetch de labels

---
+++ src/fetchs/fetchLabel.tsx
@@ -0,0 +1,28 @@
+import { apiClient } from "@/api/apiClient";
+import type { Label } from "@/pages/labels";
+
+export const fetchLabels = async () => {
+    return apiClient<Label[]>(`/labels`, {
+        method: "GET",
+    });
+}
+
+export const createLabel = async(nome: string) => {
+    return apiClient(`/labels`, {
+        method: "POST",
+        body: JSON.stringify({ nome }),
+    });
+}
+
+export const deleteLabel = async(id: number) => {
+    return apiClient(`/labels/${id}`, {
+        method: "DELETE",
+    });
+}
+
+export const updateLabel = async(id: number, nome: string) => {
+    return apiClient(`/labels/${id}`, {
+        method: "PATCH",
+        body: JSON.stringify(nome),
+    });
+}
