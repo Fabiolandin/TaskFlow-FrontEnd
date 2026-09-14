71a9a87dc48f5d2c96b15419b0e793ed71291fa7
Fabiolandin <flandin1990@gmail.com>
2026-09-14 09:16:03 -0300

    feat: adicionand função para criar nova label atravéz do dialog

--- src/components/DialogNewLabel.tsx
+++ src/components/DialogNewLabel.tsx
@@ -1,13 +1,31 @@
+import { useState } from "react";
 import { Button } from "./ui/button";
 import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle } from "./ui/dialog";
 import { Input } from "./ui/input";
+import { toast } from "sonner";
 
 interface DialogNewLabelProps {
     open: boolean;
     onOpenChange: (open: boolean) => void;
+    onCreateLabel: (nome: string) => void;
 }
 
-const DialogNewLabel = ({ open, onOpenChange }: DialogNewLabelProps) => {
+const DialogNewLabel = ({ open, onOpenChange, onCreateLabel }: DialogNewLabelProps) => {
+
+    //state para receber os dados da label
+    const [nomeLabel, setNomeLabel] = useState("");
+
+    const handleCreateLabel = async () => {
+        if(!nomeLabel.trim()) return; //impede de criar uma label com nome vazio
+        try{
+            await onCreateLabel(nomeLabel);
+            setNomeLabel("");
+            onOpenChange(false)
+        } catch(error){
+            toast.error("Erro ao criar a label")
+        }
+    }
+
     return (
         <div>
             <Dialog open={open} onOpenChange={onOpenChange}>
@@ -19,16 +37,21 @@ const DialogNewLabel = ({ open, onOpenChange }: DialogNewLabelProps) => {
                     </DialogHeader>
 
                     <p className="text-gray-400 text-[13px]">Nome</p>
-                    <Input className="text-white bg-[#151A1E] border-[#333B44] mb-2 h-10" type="text" placeholder="ex . performance" />
-
-                    <p className="text-gray-400 text-[13px]">Cor</p>
-                    <Input className="text-white bg-[#151A1E] border-[#333B44] mb-2 h-10" type="text" placeholder="ex. Taskflow Mobile" />
-
-                    <Input className="text-white bg-[#151A1E] border-[#333B44] mb-2 h-10" type="text" placeholder="Prévia" />
+                    <Input 
+                    className="text-white bg-[#151A1E] border-[#333B44] mb-2 h-10" 
+                    type="text" 
+                    placeholder="ex . performance" 
+                    value={nomeLabel}
+                    onChange={(e) => setNomeLabel(e.target.value)}
+                    />
 
                     <DialogFooter className="bg-[#0E1012]">
                         <Button variant="ghost" className="text-gray-400" onClick={() => onOpenChange(false)}>Cancelar</Button>
-                        <Button variant="destructive">Criar Projeto</Button>
+                        <Button 
+                        variant="destructive"
+                        onClick={handleCreateLabel}
+                        disabled={!nomeLabel.trim()}
+                        >Criar Label</Button>
                     </DialogFooter>
                 </DialogContent>
             </Dialog>
